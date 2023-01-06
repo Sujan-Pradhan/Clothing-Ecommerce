11a08bfc060e9db00e543ffd204a6ab7996f3a3e
Sujan <pradhansujan730@gmail.com>
2023-01-06 17:40:57 +0545

    Make use of Observer Pattern like onAuthStateChanged

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -7,9 +8,20 @@ export const UserContext = createContext({
 });
 
 export const UserProvider = ({ children }) => {
-//   console.warn("SujanChildren", children);
+  //   console.warn("SujanChildren", children);
   const [currentUser, setCurrentUser] = useState(null);
 
   const value = { currentUser, setCurrentUser };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      // console.log("Sujan", user);
+      if (user) {
+        createUserDocumentFromAuth(user)
+      }
+      setCurrentUser(user)
+    });
+    return unsubscribe;
+  }, []);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
