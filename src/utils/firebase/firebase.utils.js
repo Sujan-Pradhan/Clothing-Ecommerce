2fb832a65bd7b4bd584eba94c4e9268265c988fa
Sujan <pradhansujan730@gmail.com>
2023-01-21 21:29:58 +0545

    Add organize data for firebase database storage

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -13,7 +13,14 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  getFirestore,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCnD4lBUauHkmR-NrCJjFjjp8qpqNGfgh8",
@@ -51,6 +58,20 @@ export const signInWithGoogleRedirect = () =>
 //   signInWithPopup(auth, twitterProvider);
 
 export const db = getFirestore();
+
+export const addCollectionDocuments = async(collectionKey, objectsToAdd)=>{
+  const collectionRef = collection(db,collectionKey); 
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object)=>{
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+   
+  });
+
+  await batch.commit();
+  console.log("Done")
+}
 
 export const createUserDocumentFromAuth = async (
   userAuth,
